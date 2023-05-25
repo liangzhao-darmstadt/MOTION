53d2ea0f821527dc2476568cc45ac023268bfa31
liangzhao-darmstadt <liangzhao92@gmail.com>
2023-05-25 09:34:20 +0200

    test

--- src/examples/benchmark_liangzhao_discrete_laplace_mechanism_CKS/benchmark_liangzhao_discrete_laplace_mechanism_CKS_main.cpp
+++ src/examples/benchmark_liangzhao_discrete_laplace_mechanism_CKS/benchmark_liangzhao_discrete_laplace_mechanism_CKS_main.cpp
@@ -178,20 +178,20 @@ std::vector<Combination> GenerateAllCombinations() {
     //                           T::kDiscreteLaplaceMechanismCKS_FL32DiscreteLaplace_perturbation,
     //                           batch_size, failure_probability_pow2_neg_40);
 
-    // no overflow
-    combinations.emplace_back(
-        64, encrypto::motion::MpcProtocol::kGarbledCircuit,
-        T::kDiscreteLaplaceMechanismCKS_FL64DiscreteLaplace_noise_generation_naive, batch_size,
-        failure_probability_pow2_neg_40);
+    // // no overflow
+    // combinations.emplace_back(
+    //     64, encrypto::motion::MpcProtocol::kGarbledCircuit,
+    //     T::kDiscreteLaplaceMechanismCKS_FL64DiscreteLaplace_noise_generation_naive, batch_size,
+    //     failure_probability_pow2_neg_40);
 
-    combinations.emplace_back(
-        64, encrypto::motion::MpcProtocol::kGarbledCircuit,
-        T::kDiscreteLaplaceMechanismCKS_FL64DiscreteLaplace_noise_generation_optimized, batch_size,
-        failure_probability_pow2_neg_40);
+    // combinations.emplace_back(
+    //     64, encrypto::motion::MpcProtocol::kGarbledCircuit,
+    //     T::kDiscreteLaplaceMechanismCKS_FL64DiscreteLaplace_noise_generation_optimized, batch_size,
+    //     failure_probability_pow2_neg_40);
 
-    combinations.emplace_back(64, encrypto::motion::MpcProtocol::kGarbledCircuit,
-                              T::kDiscreteLaplaceMechanismCKS_FL64DiscreteLaplace_perturbation,
-                              batch_size, failure_probability_pow2_neg_40);
+    // combinations.emplace_back(64, encrypto::motion::MpcProtocol::kGarbledCircuit,
+    //                           T::kDiscreteLaplaceMechanismCKS_FL64DiscreteLaplace_perturbation,
+    //                           batch_size, failure_probability_pow2_neg_40);
 
     // ================================================
     // ! Garbled Circuit
